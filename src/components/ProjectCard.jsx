c37270bb61f8050c5b188e3527a7cfc3dc31cf49
Angela Lee <angelalee@Angelas-iMac.local>
2024-02-06 11:48:24 -0600

    add info for projects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -13,13 +13,13 @@ const Projects = ({ url, img, github, title, text }) => {
       <div className="capitalize p-8">
         <h2 className="tracking-wide text-xl font-medium">{title}</h2>
         <p className="mt-4 text-slate-700 leading-relaxed">{text}</p>
-        <div className="mt-4 flex gap-x-4">
-          <a href={url}>
-            <TbWorldWww className="social-link" />
+        <div className="mt-4 flex gap-x-4 items-center">
+          <a href={url} className="p-2 rounded-md bg-slate-800 text-white">
+            Live Site
           </a>
-          <a href={github}>
+          {/* <a href={github}>
             <FaGithubSquare className="social-link" />
-          </a>
+  </a>*/}
         </div>
       </div>
     </article>
